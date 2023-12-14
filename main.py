dd2e7f906e85217660f113b58d507e21af480404
Xurxo Rigueira Díaz <xrigueiradiaz@gmail.com>
2023-12-13 17:29:54 -0700

    define steps

--- main.py
+++ main.py
@@ -15,6 +15,114 @@ import utils
 import dataset as ds
 from models.autoformer import Autoformer
 
+def train(dataloader, model, loss_function, optimizer, patience, device, df_training, epoch):
+    size = len(dataloader.dataset)
+    model.train()
+    training_loss = [] # For plotting purposes
+    for i, batch in enumerate(dataloader):
+        src, tgt, src_pe, tgt_pe = batch
+        src, tgt, src_pe, tgt_pe = src.float().to(device), tgt.float().to(device), src_pe.float().to(device), tgt_pe.float().to(device)
+        
+        # Zero out gradients for every batch
+        model.zero_grad()
+        
+        # Process decoder input
+        decoder_input = torch.zeros_like(tgt[:, -output_sequence_length:, :]).float()
+        decoder_input = torch.cat([tgt[:, :decoder_sequence_len, :], decoder_input], dim=1).float().to(device)
+        
+        # Compute prediction error
+        pred, attention_weights = model(src, decoder_input, src_pe, tgt_pe)
+        
+        f_dim = -1 if features == 'MS' else 0
+        pred = pred[:, -decoder_sequence_len:, f_dim:].to(device)
+        tgt_y = tgt[:, -decoder_sequence_len:, f_dim:].to(device)
+        
+        loss = loss_function(pred, tgt_y)
+        
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        
+        # Save results for plotting
+        training_loss.append(loss.item())
+        epoch_train_loss = np.mean(training_loss)
+        df_training.loc[epoch] = [epoch, epoch_train_loss]
+        
+        if i % 5 == 0:
+            print('Current batch', i)
+            loss, current = loss.item(), (i + 1) * len(src)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_function, patience, device, df_testing, epoch):
+    num_batches = len(dataloader)
+    model.eval()
+    testing_loss = [] # For plotting purposes
+    with torch.no_grad():
+        for batch in dataloader:
+            src, tgt, src_pe, tgt_pe = batch
+            src, tgt, src_pe, tgt_pe = src.float().to(device), tgt.float().to(device), src_pe.float().to(device), tgt_pe.float().to(device)
+            
+            # Process decoder input
+            decoder_input = torch.zeros_like(tgt[:, -output_sequence_length:, :]).float()
+            decoder_input = torch.cat([tgt[:, :decoder_sequence_len, :], decoder_input], dim=1).float().to(device)
+            
+            # Compute prediction error
+            pred, attention_weights = model(src, decoder_input, src_pe, tgt_pe)
+            
+            f_dim = -1 if features == 'MS' else 0
+            pred = pred[:, -decoder_sequence_len:, f_dim:].to(device)
+            tgt_y = tgt[:, -decoder_sequence_len:, f_dim:].to(device)
+        
+            loss = loss_function(pred, tgt_y)
+            
+            # Save results for plotting
+            testing_loss.append(loss.item())
+            epoch_test_loss = np.mean(testing_loss)
+            df_testing.loc[epoch] = [epoch, epoch_test_loss]
+    
+    loss /= num_batches
+    # print(f"Avg test loss: {loss:>8f}")
+
+def validation(dataloader, model):
+    
+    # Define lists to store the predictions and ground truth
+    y_hats = []
+    tgt_ys = []
+    
+    model.eval()
+    with torch.no_grad():
+        for i, batch in enumerate(validation_data):
+            rc, tgt, src_pe, tgt_pe = batch
+            src, tgt, src_pe, tgt_pe = src.float().to(device), tgt.float().to(device), src_pe.float().to(device), tgt_pe.float().to(device)
+            
+            # Process decoder input
+            decoder_input = torch.zeros_like(tgt[:, -output_sequence_length:, :]).float()
+            decoder_input = torch.cat([tgt[:, :decoder_sequence_len, :], decoder_input], dim=1).float().to(device)
+            
+            # Compute prediction error
+            pred, attention_weights = model(src, decoder_input, src_pe, tgt_pe)
+            
+            f_dim = -1 if features == 'MS' else 0
+            pred = pred[:, -decoder_sequence_len:, f_dim:].to(device)
+            tgt_y = tgt[:, -decoder_sequence_len:, f_dim:].to(device)
+            
+            y_hat = pred.detach().cpu().numpy()
+            tgt_y = tgt_y.detach().cpu().numpy()
+            
+            y_hats.append(y_hat)
+            tgt_ys.append(tgt_y)
+    
+    y_hats = np.concatenate(y_hats, axis=0)
+    tgt_y = np.concatenate(tgt_ys, axis=0)
+    print('Validation shape:', y_hats.shape, tgt_ys.shape)
+    y_hats = y_hats.reshape(-1, y_hats.shape[-2], y_hats.shape[-1])
+    tgt_y = tgt_y.reshape(-1, tgt_y.shape[-2], tgt_y.shape[-1])
+    print('Validation shape:', y_hats.shape, tgt_ys.shape)
+    
+    # Get metrics
+    mae, mse, rmse, mape, mspe = utils.metric(y_hats, tgt_ys)
+    print('MAE: {}\nMSE: {}\nRMSE: {}\nMAPE: {}\nMSPE: '.format(mae, mse, rmse, mape, mspe))
+
 if __name__ == '__main__':
     
     # Define seed
@@ -32,9 +140,9 @@ if __name__ == '__main__':
     encoder_sequence_len = 96 # length of input given to encoder
     decoder_sequence_len = 48 # length of input given to decoder
     output_sequence_length = 96 # target sequence length. If hourly data and length = 48, you predict 2 days ahead
-    encoder_input_size = 7
-    decoder_input_size = 7
-    decoder_output_size = 7 
+    encoder_input_size = 1
+    decoder_input_size = 1
+    decoder_output_size = 1 
     encoder_features_fc_layer = 2048
     decoder_features_fc_layer = 2048
     n_encoder_layers = 2
@@ -46,11 +154,12 @@ if __name__ == '__main__':
     attention_factor = 1
     frequency = 'D'
     dropout = 0.05
-    output_attention = True
+    output_attention = True # Keep True for now
     moving_average = 25
     
     num_workers = 8
     features = 'MS' # forecasting task, options:[M, S, MS]; M:multivariate predict multivariate, S:univariate predict univariate, MS:multivariate predict univariate'
+    patience = 3
     
     # Get device
     device = ('cuda' if torch.cuda.is_available() else 'cpu')
@@ -119,4 +228,45 @@ if __name__ == '__main__':
                 n_decoder_layers=n_decoder_layers, activation=activation, embed=embed, d_model=d_model, n_heads=n_heads, attention_factor=attention_factor,
                 frequency=frequency, dropout=dropout, output_attention=output_attention, moving_average=moving_average).float()
     
+    # Send model to device
+    model.to(device)
     
+    # Print model and number of parameters
+    print('Defined model:\n', model)
+    utils.count_parameters(model)
+    
+    # Define optimizer and loss function
+    loss_function = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    
+    # Update model in the training process and test it
+    epochs = 2 # 250
+    start_time = time.time()
+    df_training = pd.DataFrame(columns=('epoch', 'loss_train'))
+    df_testing = pd.DataFrame(columns=('epoch', 'loss_test'))
+    for t in range(epochs):
+        print(f"Epoch {t+1}\n-------------------------------")
+        train(training_data, model, loss_function, optimizer, patience, device, df_training, epoch=t)
+        test(testing_data, model, loss_function, device, df_testing, epoch=t)
+    print("Done! ---Execution time: %s seconds ---" % (time.time() - start_time))
+    
+    # # Save the model
+    # torch.save(model, "models/model.pth")
+    # print("Saved PyTorch entire model to models/model.pth")
+
+    # # Load the model
+    # model = torch.load("models/model.pth").to(device)
+    # print('Loaded PyTorch model from models/model.pth')
+    
+    # Inference
+    validation(validation_data, model)
+    
+    # Plot loss
+    plt.figure(1);plt.clf()
+    plt.plot(df_training['epoch'], df_training['loss_train'], '-o', label='loss train')
+    plt.plot(df_training['epoch'], df_testing['loss_test'], '-o', label='loss test')
+    plt.yscale('log')
+    plt.xlabel(r'epoch')
+    plt.ylabel(r'loss')
+    plt.legend()
+    plt.show()
--- models/autoformer.py
+++ models/autoformer.py
@@ -93,17 +93,17 @@ class Autoformer(nn.Module):
             projection=nn.Linear(d_model, decoder_output_size, bias=True)
         )
 
-    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,
+    def forward(self, x_enc, x_dec, x_mark_enc, x_mark_dec,
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
         
         # decomp init
-        mean = torch.mean(x_enc, dim=1).unsqueeze(1).repeat(1, self.pred_len, 1)
-        zeros = torch.zeros([x_dec.shape[0], self.pred_len, x_dec.shape[2]], device=x_enc.device)
+        mean = torch.mean(x_enc, dim=1).unsqueeze(1).repeat(1, self.output_sequence_len, 1)
+        zeros = torch.zeros([x_dec.shape[0], self.output_sequence_len, x_dec.shape[2]], device=x_enc.device)
         seasonal_init, trend_init = self.decomp(x_enc)
         
         # decoder input
-        trend_init = torch.cat([trend_init[:, -self.label_len:, :], mean], dim=1)
-        seasonal_init = torch.cat([seasonal_init[:, -self.label_len:, :], zeros], dim=1)
+        trend_init = torch.cat([trend_init[:, -self.decoder_sequence_len:, :], mean], dim=1)
+        seasonal_init = torch.cat([seasonal_init[:, -self.decoder_sequence_len:, :], zeros], dim=1)
         
         # enc
         enc_out = self.enc_embedding(x_enc, x_mark_enc)
@@ -117,6 +117,6 @@ class Autoformer(nn.Module):
         dec_out = trend_part + seasonal_part
 
         if self.output_attention:
-            return dec_out[:, -self.pred_len:, :], attns
+            return dec_out[:, -self.decoder_sequence_len:, :], attns
         else:
-            return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+            return dec_out[:, -self.decoder_sequence_len:, :]  # [B, L, D]
